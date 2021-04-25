9c8aaeca1ee780ec2f7ceaf1e86d95469331f3da
Christian M <chriamue@gmail.com>
2021-04-25 19:09:47 +0200

    :sparkles: :lipstick: updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>About Lenna Page</h1>
+  <div class="about">
+    <h1>About Lenna Project</h1>
+    <p>
+      Lenna is a library for image processing algorithms. The web page allows to
+      test and use them.
+    </p>
+    <h2>Plugins</h2>
+    <p>
+      Lenna allows to plug in more algorithms. Some examples can be found at
+      <a href="https://github.com/lenna-project/lenna-plugins"
+        >lenna-project/lenna-plugins</a
+      >
+      on github.
+    </p>
+    <h2>Command Line Interface</h2>
+    <p>
+      A command line interface allows to use the algorithms and run them on
+      multiple images at once. The source for
+      <a href="https://github.com/lenna-project/lenna-cli">lenna-cli</a>
+      can be found on github.
+    </p>
+  </div>
 </template>
+<style scoped lang="css">
+.about {
+  padding: 100px;
+  padding-left: 30%;
+  background-color: #efcda4;
+  height: 100%;
+}
+</style>
