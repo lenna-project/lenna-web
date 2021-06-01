31db3cd067ec2f3a92296791dcb2197023606725
Christian M <chriamue@gmail.com>
2021-06-01 18:25:45 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Navbar />
   <router-view />
+  <Footer />
 </template>
 <script>
 import { defineComponent } from "vue";
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default defineComponent({
   name: "App",
   components: {
     Navbar,
+    Footer
   },
 });
 </script>
