8e07e9e54b70f9b0ee24b782e8101ceb96309444
Christian M <chriamue@gmail.com>
2021-08-29 16:57:58 +0200

    :poop: change path from @ to ..

--- src/views/About.vue
+++ src/views/About.vue
@@ -61,7 +61,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Help from "@/components/Help.vue";
+import Help from "../components/Help.vue";
 
 export default defineComponent({
   name: "About",
